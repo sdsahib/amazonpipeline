8050fde6f3624774d2338b9c750e8b0da03756b7
Rajshekhar Paul <v-rapaul@expedia.com>
2018-08-19 00:51:30 +0530

    Fixed the CodeDeploy execution failures

--- src/main/java/com/tekops/system/Constants.java
+++ src/main/java/com/tekops/system/Constants.java
@@ -9,5 +9,7 @@ public class Constants {
   public static final String CODE_BUILD_IMAGE_NAME = "aws/codebuild/java:openjdk-8";
   public static final String SEPERATOR_CHAR = "_";
   public static final String CODE_BUILD_SERVICE_ROLE_ARN = "arn:aws:iam::299024323439:role/CodeBuildServiceRole-latest";
+  public static final String S3_BUCKET_PATH = "MyArtifacts";
+//  public static final String EB_STACK_NAME = "64bit Amazon Linux 2018.03 v3.0.2 running Tomcat 8.5 Java 8";
   private Constants(){}
 }
