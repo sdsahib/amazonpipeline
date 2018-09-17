d9f8ddec32331736b8f63f366ae719150838f7c0
Ravi Kumar <v-ravikumar@expedia.com>
2018-09-17 13:32:14 +0530

    CORS enabled for controllers

--- src/main/java/com/tekops/system/controller/CommonItemsRetrieverController.java
+++ src/main/java/com/tekops/system/controller/CommonItemsRetrieverController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/common-items")
+@CrossOrigin("*")
 public class CommonItemsRetrieverController {
   private final CommonItemsRetrieverService commonItemsRetrieverService;
 
